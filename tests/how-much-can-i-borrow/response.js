564f56e20f917a8ef61db65a7e1d51b1b71518b3
amit <amit@ambrdetroit.com>
2016-02-04 16:37:59 -0500

    updates

---
+++ tests/how-much-can-i-borrow/response.js
@@ -0,0 +1,25 @@
+var response = {
+	conservative : {
+		priceOfHome : 235794.26,
+		downPayment : 11228.3,
+		loanAmount : 224565.97
+	},
+	aggressive : {
+		priceOfHome : 299277.33,
+		downPayment : 14251.30,
+		loanAmount : 285026.03,
+	},
+	futureMonthlyPayment : {
+		conservative : {
+			principalAndInterest : 1040,
+			taxesAndInsurance : 0,
+			totalMonthlyPayment : 1040
+		},
+		aggressive : {
+			principalAndInterest : 1320,
+			taxesAndInsurance : 0,
+			totalMonthlyPayment : 1320
+		}
+		
+	}
+};
---
+++ tests/should-i-buy-or-rent/response.js
@@ -0,0 +1,10 @@
+var response = {
+	currentValueOfHome : 269870.71,
+	totalOwedToBank : 159002.88,
+	equityOnHome : 110867.83,
+	savingWhenRenting : -7642.42,
+	transactionalCosts : 16192.24,
+	netCostOfOwnership : 94675.59,
+	moneySavedByBuying : 101800
+};
+
