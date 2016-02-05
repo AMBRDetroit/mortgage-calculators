b40634c5ea4aececd8633825fe5a0dc48e51652e
amit <amit@ambrdetroit.com>
2016-02-05 11:28:51 -0500

    updates

--- src/components/fixed-rate-vs-arm.js
+++ src/components/fixed-rate-vs-arm.js
@@ -34,70 +34,74 @@ window.mortgageCalculators.comparefixedRateVsARM = function(args){
 	var monthsBeforeFirstAdjustment = args.monthsBeforeFirstAdjustment;
 	var monthsBetweenAdjustments = args.monthsBetweenAdjustments;
 	var maximumInterestRate = args.maximumInterestRate;
-	
-	var fixedMonthlyMortgagePayment = calculateMonthlyMortgagePayment({
-		loanAmount : loanAmount,
-		interestRate : fixedInterestRate,
-		termInYears : termInYears
-	});
-	
-	var initialMonthlyMortgagePayment = calculateMonthlyMortgagePayment({
-		loanAmount : loanAmount,
-		interestRate : initialInterestRate,
-		termInYears : termInYears
-	});
+	// if term in years and months beweent adjustment need to be greater than 0 for these loops to work, otherwise will cause infinite loop
+	if(termInYears > 0 && monthsBetweenAdjustments > 0){
+			var fixedMonthlyMortgagePayment = calculateMonthlyMortgagePayment({
+			loanAmount : loanAmount,
+			interestRate : fixedInterestRate,
+			termInYears : termInYears
+		});
+		
+		var initialMonthlyMortgagePayment = calculateMonthlyMortgagePayment({
+			loanAmount : loanAmount,
+			interestRate : initialInterestRate,
+			termInYears : termInYears
+		});
 
-	var remainingBalance = loanAmount;
-	// calculate payments before first adjustment 
-	for(var i = 0; i < monthsBeforeFirstAdjustment; i++){
-		var monthlyInterestPayment =  calculateMonthlyInterestPayment( (initialInterestRate/100)/12,remainingBalance );
-		var monthlyPrincipalPayment = initialMonthlyMortgagePayment - monthlyInterestPayment;
-		remainingBalance -= monthlyPrincipalPayment;
-	}
-	// calculate payments between adjustments 
-	var adjustedInterestRate = initialInterestRate + expectedAdjustmentRate;
-	var adjustedMonthlyMortgagePayment = calculateMonthlyMortgagePayment({
-		loanAmount : remainingBalance,
-		interestRate : adjustedInterestRate,
-		termInYears : termInYears - (monthsBeforeFirstAdjustment/12)
-	});
 
-	for(var j=0; j < monthsBetweenAdjustments; j++ ){
-		var newMonthlyInterestPayment =  calculateMonthlyInterestPayment((adjustedInterestRate/100)/12,remainingBalance);
-		var newMonthlyPrincipalPayment = adjustedMonthlyMortgagePayment - newMonthlyInterestPayment;
-		remainingBalance -= newMonthlyPrincipalPayment;
-	}
-	// calculate payments after adjustments 
-	var remainingPayments = (termInYears*12) - monthsBeforeFirstAdjustment - monthsBetweenAdjustments;
-	var maxMonthlyMortgagePayment;
-	for (var k=0; k < remainingPayments; k+=monthsBetweenAdjustments){
-		if(adjustedInterestRate < maximumInterestRate){
-			adjustedInterestRate += expectedAdjustmentRate;
-		}else{
-			adjustedInterestRate = maximumInterestRate;
+		var remainingBalance = loanAmount;
+		// calculate payments before first adjustment 
+		for(var i = 0; i < monthsBeforeFirstAdjustment; i++){
+			var monthlyInterestPayment =  calculateMonthlyInterestPayment( (initialInterestRate/100)/12,remainingBalance );
+			var monthlyPrincipalPayment = initialMonthlyMortgagePayment - monthlyInterestPayment;
+			remainingBalance -= monthlyPrincipalPayment;
 		}
-		adjustedMonthlyMortgagePayment =  calculateMonthlyMortgagePayment({
+		// calculate payments between adjustments 
+		var adjustedInterestRate = initialInterestRate + expectedAdjustmentRate;
+		var adjustedMonthlyMortgagePayment = calculateMonthlyMortgagePayment({
 			loanAmount : remainingBalance,
 			interestRate : adjustedInterestRate,
-			termInYears : (remainingPayments-k)/12
-		}); 
-		
-		for(var x=0; x < monthsBetweenAdjustments; x++){
+			termInYears : termInYears - (monthsBeforeFirstAdjustment/12)
+		});
+
+		for(var j=0; j < monthsBetweenAdjustments; j++ ){
 			var newMonthlyInterestPayment =  calculateMonthlyInterestPayment((adjustedInterestRate/100)/12,remainingBalance);
 			var newMonthlyPrincipalPayment = adjustedMonthlyMortgagePayment - newMonthlyInterestPayment;
 			remainingBalance -= newMonthlyPrincipalPayment;
 		}
-		maxMonthlyMortgagePayment = adjustedMonthlyMortgagePayment;
+		// calculate payments after adjustments 
+		var remainingPayments = (termInYears*12) - monthsBeforeFirstAdjustment - monthsBetweenAdjustments;
+		var maxMonthlyMortgagePayment;
+		for (var k=0; k < remainingPayments; k+=monthsBetweenAdjustments){
+			if(adjustedInterestRate < maximumInterestRate){
+				adjustedInterestRate += expectedAdjustmentRate;
+			}else{
+				adjustedInterestRate = maximumInterestRate;
+			}
+			adjustedMonthlyMortgagePayment =  calculateMonthlyMortgagePayment({
+				loanAmount : remainingBalance,
+				interestRate : adjustedInterestRate,
+				termInYears : (remainingPayments-k)/12
+			}); 
+			
+			for(var x=0; x < monthsBetweenAdjustments; x++){
+				var newMonthlyInterestPayment =  calculateMonthlyInterestPayment((adjustedInterestRate/100)/12,remainingBalance);
+				var newMonthlyPrincipalPayment = adjustedMonthlyMortgagePayment - newMonthlyInterestPayment;
+				remainingBalance -= newMonthlyPrincipalPayment;
+			}
+			maxMonthlyMortgagePayment = adjustedMonthlyMortgagePayment;
+		}
+		
+		response = {
+			fixedRate : {
+				monthlyMortgagePayment : formatResult(fixedMonthlyMortgagePayment)
+			},
+			ARM : {
+				initialMonthlyMortgagePayment : formatResult(initialMonthlyMortgagePayment),
+				maxMonthlyMortgagePayment : formatResult(maxMonthlyMortgagePayment)
+			}
+		}
+		return response;	
 	}
 	
-	var response = {
-		fixedRate : {
-			monthlyMortgagePayment : formatResult(fixedMonthlyMortgagePayment)
-		},
-		ARM : {
-			initialMonthlyMortgagePayment : formatResult(initialMonthlyMortgagePayment),
-			maxMonthlyMortgagePayment : formatResult(maxMonthlyMortgagePayment)
-		}
-	}
-	return response;	
 };
