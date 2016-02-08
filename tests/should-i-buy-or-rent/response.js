89a627cedb3bc5045b6124157dfc6f94c8f42ffb
amit <amit@ambrdetroit.com>
2016-02-08 10:04:07 -0500

    updated rent vs buy tests

--- tests/should-i-buy-or-rent/response.js
+++ tests/should-i-buy-or-rent/response.js
@@ -1,3 +1,4 @@
+// When down payment and closing costs are set to 0
 var response1 = {
 	  currentValueOfHome: 268783.28,
 	  totalOwedToBank: 167371.45,
@@ -6,3 +7,12 @@ var response1 = {
 	  netCostOfRenting: 120747.77,
 	  benefitOfBuying: 109421.27
 };
+// when all inputs are set to > 0
+var response2 = {
+	benefitOfBuying: 110576.76,
+	currentValueOfHome: 268783.28,
+	equityOnHome: 109780.4,
+	netCostOfBuying: 10171.01,
+	netCostOfRenting: 120747.77,
+	totalOwedToBank: 159002.88
+}
